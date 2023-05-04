8e57b23d50d3c0381c988be9ba1372b5f68157ec
Bucha1958 <okoriemmadu@gmail.com>
2023-05-04 11:46:43 +0000

    Setup

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 # Bash script that sets up your web servers
-sudo apt-get update
-sudo apt-get -y install nginx
+if ! which nginx > /dev/null; then
+	sudo apt-get update
+	sudo apt-get -y install nginx
+fi
 sudo mkdir -p /data/web_static/releases/test/
 sudo mkdir -p /data/web_static/shared/
 sudo chown -R ubuntu:ubuntu /data/

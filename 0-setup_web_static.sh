7a69e217cadf027d1a0157a883a0dc1e46d72753
Bucha1958 <okoriemmadu@gmail.com>
2023-05-04 09:24:10 +0000

    Nginx configuration

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+# Bash script that sets up your web servers
+sudo apt-get update
+sudo apt-get -y install nginx
+sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared/
+sudo chown -R ubuntu:ubuntu /data/
+echo "This is a test" | sudo tee /data/web_static/releases/test/index.html
+sudo rm -f /data/web_static/current
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo sed -i 's/server_name _;/server_name _;\n\n\tlocation \/hbnb_static {\n\t\talias \/data\/web_static\/current\/;\n\t}/' /etc/nginx/sites-available/default
+sudo service nginx restart

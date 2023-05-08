037303e0ccf2e0a38bcad9c33e56eca6a160c133
Epicsociety <measkswho@gmail.com>
2023-05-08 20:45:52 +0300

    nginx installation, folder creates, symbolic link

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+i#!/usr/bin/env bash
 # Bash script that sets up your web servers
 if ! which nginx > /dev/null; then
 	apt-get update
@@ -6,9 +6,13 @@ if ! which nginx > /dev/null; then
 fi
 mkdir -p /data/web_static/releases/test/
 mkdir -p /data/web_static/shared/
-chown -R ubuntu:ubuntu /data/
-echo "This is a test" | sudo tee /data/web_static/releases/test/index.html
-rm -f /data/web_static/current
-ln -sf /data/web_static/releases/test/ /data/web_static/current
+
+chown -R ubuntu:ubuntu /data
+
+echo "<html><head></head><body>Holberton School</body></html>" | sudo tee /data/web_static/releases/test/index.html
+rm -rf /data/web_static/current
+ln -s /data/web_static/releases/test/ /data/web_static/current
 sed -i 's/server_name _;/server_name _;\n\n\tlocation \/hbnb_static {\n\t\talias \/data\/web_static\/current\/;\n\t}/' /etc/nginx/sites-available/default
 service nginx restart
+
+exit 0

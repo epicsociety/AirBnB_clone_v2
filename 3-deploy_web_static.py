92f907c42b578e690f54ab4391755e62cde2c5c1
Bucha1958 <okoriemmadu@gmail.com>
2023-05-08 11:26:04 +0000

    Full deployment

---
+++ 3-deploy_web_static.py
@@ -0,0 +1,70 @@
+#!/usr/bin/python3
+
+import os
+import tarfile
+import datetime
+from fabric.api import *
+from fabric.operations import run, put
+from os import path
+
+env.hosts = ['54.157.174.94', '100.25.22.146']
+env.user = 'ubuntu'
+
+
+def do_pack():
+    """generates a .tgz archive"""
+    
+    try:            
+        if not path.exists("versions"):
+            local("mkdir versions")
+        now = datetime.now().strftime("%Y%m%d%H%M%S")
+        name = "web_static_" + now
+        local("tar -cvzf versions/{}.tgz web_static/".format(name))
+        size_of_file = path.getsize("./versions/{}.tgz".format(name))
+        print("web_static packed: versions/" + name + ".tgz -> " + size_of_file + " Bytes")
+    except BaseException:
+        return None
+
+
+def do_deploy(archive_path):
+    '''
+         Deploys an archive to the web servers
+    '''
+    filename = os.path.basename(archive_path)
+    if not os.path.exists(archive_path):
+         return False
+
+    result = put(archive_path, "/tmp/")
+    if result.failed:
+        return False
+    run("mkdir -p /data/web_static/releases/{}".format(filename[:-4]))
+    cmd = "tar -xzf /tmp/{} -C /data/web_static/releases/{}".format(filename, filename[:-4])
+    result = run(cmd)
+    if result.failed:
+        return False
+    result = run("rm /tmp/{}".format(filename))
+    if result.failed:
+        return False
+    run("cp -rp /data/web_static/releases/{}/web_static/*\
+                    /data/web_static/releases/{}/".format(filename[:-4], filename[:-4]))
+    run("rm -rf /data/web_static/releases/{}/web_static/".format(filename[:-4]))
+    result = run("rm /data/web_static/current")
+    if result.failed:
+        return False
+    path = "/data/web_static/releases/{}".format(filename[:-4])
+    cmd = "ln -sf {} /data/web_static/current".format(path)
+    result = run(cmd)
+    if result.failed:
+        return False
+    return True
+
+def deploy():
+    """
+        Interface for set-up functions
+    """
+    path = do_pack()
+    if path is None:
+        return False
+    else:
+        value = do_deploy(path)
+        return value

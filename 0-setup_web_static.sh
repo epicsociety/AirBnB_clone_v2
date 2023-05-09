f8b8c58804a4e0b29d9735dd916fc354d2d34716
epicsociety <measkswho@gmail.com>
2023-05-09 07:19:49 +0000

    setting up web servers for airbnb

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,7 +1,7 @@
-i#!/usr/bin/env bash
-# Bash script that sets up your web servers
+#!/usr/bin/env bash
+# Bash script that sets up the web servers
 if ! which nginx > /dev/null; then
-	apt-get update
+	apt-get -y  update
 	apt-get -y install nginx
 fi
 mkdir -p /data/web_static/releases/test/
@@ -9,10 +9,10 @@ mkdir -p /data/web_static/shared/
 
 chown -R ubuntu:ubuntu /data
 
-echo "<html><head></head><body>Holberton School</body></html>" | sudo tee /data/web_static/releases/test/index.html
+echo "Holberton School" | sudo tee /data/web_static/releases/test/index.html
 rm -rf /data/web_static/current
 ln -s /data/web_static/releases/test/ /data/web_static/current
-sed -i 's/server_name _;/server_name _;\n\n\tlocation \/hbnb_static {\n\t\talias \/data\/web_static\/current\/;\n\t}/' /etc/nginx/sites-available/default
+sed -i 's|^.*server_name.*$|    server_name _;\n    location /hbnb_static {\n        alias /data/web_static/current/;\n    }|' /etc/nginx/sites-available/default
 service nginx restart
 
 exit 0

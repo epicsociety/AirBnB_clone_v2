3f401b9e7622d7356e169f9970011e7a04610c83
epicsociety <measkswho@gmail.com>
2023-05-09 07:45:51 +0000

    setting up web servers for airbnb

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -17,7 +17,7 @@ echo "<html>
     </body>
 </html>" | sudo tee /data/web_static/releases/test/index.html
 rm -rf /data/web_static/current
-ln -s /data/web_static/releases/test/ /data/web_static/current
+ln -s /data/web_static/releases/test /data/web_static/current
 sed -i 's|^.*server_name.*$|    server_name _;\n    location /hbnb_static {\n        alias /data/web_static/current/;\n    }|' /etc/nginx/sites-available/default
 service nginx restart
 

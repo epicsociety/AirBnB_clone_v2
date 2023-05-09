c7072dd9ffa2e0d89011a86085d0d98f51f81521
epicsociety <measkswho@gmail.com>
2023-05-09 07:31:44 +0000

    setting up web-servers for airbnb project

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -9,7 +9,13 @@ mkdir -p /data/web_static/shared/
 
 chown -R ubuntu:ubuntu /data
 
-echo "Holberton School" | sudo tee /data/web_static/releases/test/index.html
+echo "<html>
+    <head>
+    </head>
+    <body>
+        Holberton School
+    </body>
+</html>" | sudo tee /data/web_static/releases/test/index.html
 rm -rf /data/web_static/current
 ln -s /data/web_static/releases/test/ /data/web_static/current
 sed -i 's|^.*server_name.*$|    server_name _;\n    location /hbnb_static {\n        alias /data/web_static/current/;\n    }|' /etc/nginx/sites-available/default

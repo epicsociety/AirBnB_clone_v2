bd3a57d4945dff7e9817eb3a668bfaed520ea34f
epicsociety <measkswho@gmail.com>
2023-05-09 08:09:34 +0000

    script fixed

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -7,8 +7,6 @@ fi
 mkdir -p /data/web_static/releases/test/
 mkdir -p /data/web_static/shared/
 
-chown -R ubuntu:ubuntu /data
-
 echo "<html>
     <head>
     </head>
@@ -18,6 +16,9 @@ echo "<html>
 </html>" | sudo tee /data/web_static/releases/test/index.html
 rm -rf /data/web_static/current
 ln -s /data/web_static/releases/test /data/web_static/current
+
+chown -R ubuntu:ubuntu /data/
+
 sed -i 's|^.*server_name.*$|    server_name _;\n    location /hbnb_static {\n        alias /data/web_static/current/;\n    }|' /etc/nginx/sites-available/default
 service nginx restart
 

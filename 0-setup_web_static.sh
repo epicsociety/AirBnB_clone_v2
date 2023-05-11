38496c71d177967d6baf3e62d5f22db28bbf8379
epicsociety <measkswho@gmail.com>
2023-05-11 19:35:20 +0000

    set up webservers

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -8,11 +8,11 @@ mkdir -p /data/web_static/releases/test/
 mkdir -p /data/web_static/shared/
 
 echo "<html>
-    <head>
-    </head>
-    <body>
-      Holberton School
-    </body>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
 </html>" | sudo tee /data/web_static/releases/test/index.html
 rm -rf /data/web_static/current
 ln -s /data/web_static/releases/test /data/web_static/current

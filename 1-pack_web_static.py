5d011bc78fcc7936b893edda94fbcfce2fb51648
Epicsociety <measkswho@gmail.com>
2023-05-12 13:59:55 +0300

    add this

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,19 +1,25 @@
+#!/usr/bin/python3
+""" Module use fabric lib to compress a folder to .tgz """
+
 from fabric.api import local
 from datetime import datetime
 
 def do_pack():
-    """ Generate a .tgz archibe from the contents of the given webstatic folder"""
-    # Create the version folder
-    local("mkdir -p versions")
+    """ Generate a .tgz archive from the contents of the given webstatic folder
 
-    # generate the archive path using 2023-05-11 23:46
-    timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
-    archive_path = "versions/web_static_{}.tgz".format(timestamp)
+    Returns:
+        path to the archive
+    """
+    try:
+        local("mkdir -p versions;", capture=True)
+        # generate the archive path using 2023-05-11 23:46
+        timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
+        archive_path = "versions/web_static_{}.tgz".format(timestamp)
 
-    command  = "tar -czvf {} web_static".format(archive_path)
-    result = local(command, capture=True)
+        command  = "tar -czvf {} web_static".format(archive_path)
+        local(command)
 
-    if result.succeeded:
         return archive_path
-    else:
+
+    except Exception:
         return None

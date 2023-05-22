4df31de4ad02a955de1f0f2d95da478e1aa74bf4
Bucha1958 <okoriemmadu@gmail.com>
2023-05-22 02:24:21 +0000

    Flask script

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -19,8 +19,8 @@ def hello_hbnb():
 @app.route("/c/<text>", strict_slashes=False)
 def c_text(text):
     replace = text.replace("_", " ")
-    return ('C' + replace)
+    return ('C ' + replace)
 
 
 if __name__ == "__main__":
-    app.run("0.0.0.0")
+    app.run(host="0.0.0.0", port=5000)
